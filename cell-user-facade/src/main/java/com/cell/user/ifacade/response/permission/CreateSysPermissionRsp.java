3faa735164a8c98ff720c4278c951b22dffebecd
TonyYang9527 <leetony554@gmail.com>
2017-03-31 21:37:56 +0800

    permission request code add

--- cell-user-facade/src/main/java/com/cell/user/ifacade/response/permission/CreateSysPermissionRsp.java
+++ cell-user-facade/src/main/java/com/cell/user/ifacade/response/permission/CreateSysPermissionRsp.java
@@ -2,19 +2,16 @@ package com.cell.user.ifacade.response.permission;
 
 import com.cell.user.ifacade.response.BaseResponse;
 
-public class CreateSysPermissionRsp extends BaseResponse{
+public class CreateSysPermissionRsp extends BaseResponse {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3050188394443763876L;
-	  private Long id;
+	private Long id;
 
-	    public Long getId() {
-	        return id;
-	    }
+	public Long getId() {
+		return id;
+	}
 
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
